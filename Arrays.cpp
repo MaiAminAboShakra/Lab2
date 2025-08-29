9276296b2ec781d8124881c0291333eced364e42
Mai Abo-Shakra <shakram@nmsu.edu>
2025-08-29 12:03:44 -0600

    First commit

---
+++ Arrays.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+//add a class here if needed
+
+//Step1: create printValues(int vals[], int count) here
+//print the numbers you read from the user 10 per line.
+//count the numbers and print the total ccount only one time. 
+
+
+//Step2: create sumSelected(int vals[], int count, int offset, int step) here
+//find the total of a selected segment of the array
+//format the out put as in the example 
+//Total sum of all values is: ###
+//Sum of even-indexed values is: ###
+//um of odd-indexed values is: ###.
+
+
+// Step 3: Create a function 
+//int searchValue(int values[], int count, int target) 
+//that searches the values for a given value(target),
+// and returns the index where it is at, or -1 if it is not found.
+
+
+int main() {
+    const int MAX_SIZE = 100;
+    int vals[MAX_SIZE] = {0};
+    int count = 0;
+
+    // Step 1: Read user input
+    cout << "Enter up to 100 non-zero integer values, one per line, "
+         << "then enter 0 to stop, and hit Enter:" << endl;
+
+    //add the calls to all needed functions
+    //write code here to do a repeated search as described in step 3. 
+
+        return 0;
+}
---
+++ Shapes_printing.cpp
@@ -0,0 +1,28 @@
+/**
+ * Assignment: Print the given star pattern in the assignment.
+ * Author: [Your Name]
+ * - Keep your code clean and well-documented.
+ * - Push your solution to GitHub when done.
+ */
+
+#include <iostream>
+using namespace std;
+
+// Implement the function to print the triangle part of the given pattern
+void printTriangle(int height) {
+    
+}
+
+// Implement the function to print the rectangle part of the given pattern
+void printRectangle(int width, int height) {
+}
+
+int main() {    
+    cout<< "Printing the star pattern:\n\n";
+
+    // Complete the functions and fix the errors to print the pattern
+    printTriangle(triangleHeight);
+    printRectangle(rectWidth, rectHeight);
+
+    return 0;
+}
